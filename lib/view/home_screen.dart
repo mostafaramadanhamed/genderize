2a479761587b75f54ae8dfbc7308969d4187e7b4
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-02-06 13:20:33 +0200

    ui for home screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -32,12 +32,43 @@ class _HomeScreenState extends State<HomeScreen> {
                   if(formKey.currentState!.validate()){
                     formKey.currentState!.save();
                     AppCubit().getData(name: va);
+
                   }
                   else{
                     autovalidateMode=AutovalidateMode.always;
                     setState((){});
                   }
-              },)
+              },),
+              SizedBox(
+                height: MediaQuery.of(context).size.height/10,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                padding: const EdgeInsets.all( 24),
+                height:MediaQuery.of(context).size.height/3 ,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.pink.shade200,
+                  borderRadius: BorderRadius.circular(16)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text('Name:'),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height/20,
+                    ),
+                    Row(
+                      children:const [
+
+                         Expanded(child: Text('Gender:')),
+                         Expanded(child:  Text('Probability')),
+                      ],
+                    ),
+
+                  ],
+                ),
+              )
             ],
           ),
         ),
