e36dc388e62ca230050dc8bedad5ed8ac0fb52e5
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-02-06 12:02:21 +0200

    add cubit

---
+++ lib/cubit/app_states.dart
@@ -0,0 +1,5 @@
+abstract class AppStates{}
+class AppInitialState extends AppStates{}
+class AppLoadingState extends AppStates{}
+class AppSuccessState extends AppStates{}
+class AppErrorState extends AppStates{}
